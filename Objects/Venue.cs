53b7cd761a272596f7cfbe1e73ae4b3ab09ba960
Brittany Rausch <berausch247@gmail.com>
2016-07-22 09:41:29 -0700

    Creates a basic file structure for the band tracker app, including a Venue object with parameters, a constructor, and a GetAll method, and a VenueTest with the first test

---
+++ Objects/Venue.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace bandTracker
+{
+  public class Venue
+  {
+    private int _id;
+    private string _name;
+
+    public Venue(string Name, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+    }
+
+    public override bool Equals(System.Object otherVenue)
+    {
+      if(!(otherVenue is Venue))
+      {
+        return false;
+      }
+      else
+      {
+        Venue newVenue = (Venue) otherVenue;
+        bool idEquality = this.GetId() == newVenue.GetId();
+        bool nameEquality = this.GetName() == newVenue.GetName();
+        return (idEquality && nameEquality);
+      }
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public static List<Venue> GetAll()
+    {
+      List<Venue> allVenues = new List<Venue>{};
+
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM venues", conn)
+      rdr = ExecuteReader();
+
+      While(rdr.Read())
+      {
+        int venueId = rdr.GetInt32(0);
+        string venueName = rdr.GetString(1);
+        Venue newVenue = Venue(venueName, venueId);
+        allVenues.Add(newVenue);
+      }
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+      return allVenues;
+    }
+  }
+}
