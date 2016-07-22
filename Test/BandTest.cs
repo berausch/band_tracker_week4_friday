23cc581908142d73130e6b97810d61a83ec915d0
Brittany Rausch <berausch247@gmail.com>
2016-07-22 16:04:58 -0700

    Add the Save and DeletAll methods and the associated tests for the band table

--- Test/BandTest.cs
+++ Test/BandTest.cs
@@ -19,10 +19,38 @@ namespace bandTracker
         int result = Band.GetAll().Count;
         Assert.Equal(0, result);
     }
+    [Fact]
+    public void Test_Equal_ReturnsTrueForSameName()
+    {
+      Band firstBand = new Band("Queen");
+      Band secondBand = new Band ("Queen");
+
+      Assert.Equal(firstBand, secondBand);
+    }
+    [Fact]
+    public void Test_Save_SavesBandToDatabase()
+    {
+      Band testBand = new Band("Queen");
+      testBand.Save();
+      List<Band> result = Band.GetAll();
+      List<Band> testBand = new List<Band>{"Queen"};
+      Assert.Equal(testBand, result);
+    }
+    [Fact]
+    public void Test_Save_AssignsIdtoBandObject()
+    {
+      Band testBand = new Band("Queen");
+      newBand.Save();
+      Band savedBand = Band.GetAll()[0];
+      int result = testBand.GetId();
+      int testId = savedBand.GetId();
+
+      Assert.Equal(result, testId);
+    }
     public void Dispose()
     {
       Venue.DeleteAll();
-      // Band.DeleteAll();
+      Band.DeleteAll();
     }
   }
 }
